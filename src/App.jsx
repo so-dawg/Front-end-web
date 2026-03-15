c652d070ac32211b34f3580b43735fc28e4e61e0
so-dawg <meychansopheaktra@gmail.com>
2026-03-15 20:39:37 +0700

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <p>hello world!</p>
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p className="text-3xl font-bold text-blue-600">hello world!</p>
+    </div>
   );
 }
 
