843f8ffb610ce39cd33d2f58f76660bd0c358a91
so-dawg <meychansopheaktra@gmail.com>
2026-03-30 06:13:14 +0700

    write example tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <p className="text-3xl font-bold text-blue-600">hello world!</p>
+    <div className="main">
+      <p className="main1">hello world!</p>
     </div>
   );
 }
